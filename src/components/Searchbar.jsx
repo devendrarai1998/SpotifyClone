27b82406a2d165e6efcfee4837f443426c5197bc
Devendra Kumar Rai <68963294+devendrarai1998@users.noreply.github.com>
2023-01-31 02:07:52 +0530

    first commit

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FiSearch } from 'react-icons/fi';
+
+const Searchbar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    navigate(`/search/${searchTerm}`);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      autoComplete="off"
+      className="p-2 text-gray-400 focus-within:text-gray-600"
+    >
+      <label htmlFor="search-field" className="sr-only">
+        Search all Songs
+      </label>
+      <div className="flex flex-col justify-start items-center">
+        <FiSearch aria-hidden="true" className="w-5 h-5 ml-4" />
+        <input
+          name="search-field"
+          autoComplete="off"
+          id="search-field"
+          placeholder="Search"
+          type="search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="flex-1 bg-transparent border-none
+        placeholder-gray-500 outline-none text-base text-white p-4"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default Searchbar;
